ad017edff64f255b11fa0e3b5a298f60c6d91cb3
Prajna R Bhat <121172069+prajnarbhat@users.noreply.github.com>
2025-02-06 22:00:24 +0530

    Add files via upload

---
+++ Program4-practice/script.js
@@ -0,0 +1,11 @@
+const cardsPerPage = 4; // Number of cards to show per page 
+const dataContainer = document.getElementById('data-container'); 
+const pagination = document.getElementById('pagination'); 
+const prevButton = document.getElementById('prev'); 
+const nextButton = document.getElementById('next'); 
+const pageNumbers = document.getElementById('page-numbers'); 
+const pageLinks = document.querySelectorAll('.page-link'); 
+
+
+
+
